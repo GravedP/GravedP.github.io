502007f08af83e749a67d9b31d2d200ebf57a10b
Danny <skyfall100@aol.de>
2026-05-24 07:26:02 +0200

    Neu

--- script.js
+++
@@ -1,9 +0,0 @@
-// Smooth Scroll für interne Links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
-    });
-  });
